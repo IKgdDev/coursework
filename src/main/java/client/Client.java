11e4298c93ae11fd5873864868b21c1aee88a8f1
Aleksandr Desyatnichenko <alik_kenig@rambler.ru>
2022-11-04 16:40:45 +0200

    Add files *.json

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,9 +1,6 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
@@ -13,12 +10,14 @@ public class Client {
         try (
                 Socket clientSocket = new Socket(host, port);
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter outFile = new PrintWriter("reply.json");
+                BufferedReader inFile = new BufferedReader(new FileReader("request.json"))
         ) {
 
-            out.println("{\"title\": \"мыло\", \"date\": \"2022.04.09\", \"sum\": 1000}");
+            out.println(inFile.readLine());
 
-            System.out.println(in.readLine());
+            outFile.println(in.readLine());
         }
     }
 }
